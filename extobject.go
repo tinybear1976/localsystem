92a8780cc00f0255ccf8cb974d8d2ab1e0600e1b
tinybear1976 <908247013@qq.com>
2022-01-18 13:43:54 +0800

    修正CopyObject

--- extobject.go
+++ extobject.go
@@ -2,7 +2,8 @@ package localsystem
 
 import "encoding/json"
 
-func CopyObject(source interface{}, dest interface{}) {
+func CopyObject(source interface{}, dest interface{}) interface{} {
 	sbytes, _ := json.Marshal(source)
 	json.Unmarshal(sbytes, dest)
+	return dest
 }
