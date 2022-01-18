b8f460e5df2a5d4cd8a7f491163f4d77b6375a24
tinybear1976 <908247013@qq.com>
2022-01-18 13:51:32 +0800

    修正CopyObject函数

--- extobject.go
+++ extobject.go
@@ -2,8 +2,8 @@ package localsystem
 
 import "encoding/json"
 
-func CopyObject(source interface{}, dest interface{}) interface{} {
+// 目标dest对象因为需要返回，所以需要取地址传入
+func CopyObject(source interface{}, dest interface{}) {
 	sbytes, _ := json.Marshal(source)
 	json.Unmarshal(sbytes, dest)
-	return dest
 }
